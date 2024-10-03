6dfa8bf99e102026df74e6a3c3348b40002d724f
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2024-10-03 14:32:34 +0200

    corrections

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@
 // Je commence ce quizz à 20: 10
 
 // 1. Expliquez ce qu'est-ce qu'une variable.
-// REPONSE: une variable est une manière de prendre en main un élément de l'index.html, c'est comme transformer un/plusieurs élément en un pion qu'on peut ensuite manipuler. On utilise let pour une variable qui peut changer, et const pour une variable inchangeable.
+// REPONSE: une variable est une manière de prendre en main un élément de l'index.html ou bien un chiffre ou bien avoir une chaîne de caractère, c'est comme transformer un/plusieurs élément en un pion qu'on peut ensuite manipuler. On utilise let pour une variable qui peut changer, et const pour une variable inchangeable.
 
 // -----------------------
 // 2. Déclarez une variable s'appelant "firstName" et dont la valeur est votre prénom.
@@ -16,13 +16,13 @@ const firstName = "Giulia";
 // -----------------------
 // 3. Déclarez une variable s'appelant "age" et dont la valeur est votre âge. Attention, sa valeur pourrait être modifiée par la suite.
 
-let = 19;
+let age = 19;
 
 // -----------------------
 // 4. Dans le code HTML (lignes 17 - 23), citez les divers éléments composant l'élément HTML <input>:
-// - Balise HTML: aucune, la balise est <input/>
+// - Balise HTML: <input/>
 // - Classe(s): .card-input
-// - Attribut(s): type, name
+// - Attribut(s): type, name, placeholder, id, class
 
 // -----------------------
 // 5. Expliquez la différence entre une class et un ID.
@@ -37,31 +37,29 @@ let = 19;
 // 7.1 Créez une variable appelée "cardContent".
 // 7.2 Assignez lui l'élément <p> (ligne 14 du code HTML) que vous avez sélectionné en 7.1.
 
-const cardContent = document.querySelector("#card-1");
+const cardContent = document.querySelector("#card-1 p");
 
 // -----------------------
 // 8. Utilisez la variable "cardContent" créée à l'exercice 7:
 // 8.1 Utilisez la propriété   -> .classList <-   pour ajouter   -> .add("...") <-   la class "highlight" à l'élément HTML.
+// 8.2 De même, retirez la class .cardContent
 
-function addHighlight() {
-  cardContent.classList.add("highlight");
-}
-
-addHighlight();
+cardContent.classList.add("highlight");
+cardContent.classList.remove("card-content");
 
 // -----------------------
 // 9. Récupérez la valeur de l'attribut "placeholder"   -> .getAttribute("...") <-   de l'élément HTML <input> (ligne 17 du code HTML).
 
-const getAttribute = document.querySelector("#card-2");
-function getAtt() {
-  getAttribute.getAttribute("placeholder");
-}
-
-getAtt();
+//on sélectionne l'élément dont on veut connaître l'id
+const passwordInput = document.querySelector("#password-input");
+//là on cherche l'attribut.
+passwordInput.getAttribute("placeholder");
+// pour modifier un attribut on met setAttribute, premier "valeur de l'attribut" "nouvelle valeur de l'attribut"
+passwordInput.setAttribute("placeholder", "***");
 
 // -----------------------
 // 10. Expliquez ce qu'est un écouteur d'événement.
-// REPONSE: un écouteur d'évènement réagit de la manière dont nous lui avons préalablement lorsque l'utilisateur.ice fait l'action que nous avons entré dans l'écouteur d'évènement comme par ex: "click", "scroll", etc.
+// REPONSE: c'est une fonction qui attend une action de l'utilisateurice comme un "click"/"scroll" pour exécuter des instructions.
 
 // -----------------------
 // 11. Sur l'élément HTML button (ligne 26 du code HTML), écoutez l'événement "click":
@@ -72,5 +70,6 @@ getAtt();
 const btnCard = document.querySelector("#card-btn");
 
 btnCard.addEventListener("click", function () {
-  this.classList.toggle("hide");
+  const toggleText = document.querySelector("#toggle-text");
+  toggleText.classList.toggle("hide");
 });
